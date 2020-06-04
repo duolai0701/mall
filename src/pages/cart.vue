0429cc275d3b9f9d18bb152bf3cbc9ecaf849a02
李楚玉 <2903415690@qq.com>
2020-06-04 17:28:35 +0800

    6.4

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <order-header></order-header>
+    <order-header title='我的购物车'>
+      <template v-slot:tip>
+        <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算</span>
+      </template>
+    </order-header>
     cart
     <nav-footer></nav-footer>
   </div>
@@ -12,7 +16,29 @@ export default {
   name:'cart',
   components:{
     OrderHeader,
+    ServiceBar,
     NavFooter
+  },
+  data(){
+    return {
+      list:[],//商品列表
+      allChecked:false,//是否全选
+      cartTotalPrice:0,//商品总金额
+      checkedNum:0 //选中商品数量
+    }
+  },
+  mounted(){
+    this.getCartList();
+  },
+  methods:{
+    getCartList(){
+      this.axios.get('/carts').then((res)=>{
+        this.list = res.cartProductVoList || []
+        this.allChecked = res.selectedAll
+        this.cartTotalPrice = res.cartTotalPrice
+        this.checkedNum = res.cartTotalQuantity  
+      })
+    }
   }
 }
 </script>
